0f1f2460e0c3ba259c42f035bf9360689ee7d4fe
neavo <neavo@neavo.me>
2020-01-10 17:57:23 +0800

    UnitFrame : 对各职业减伤、保命技能的监控列表进行了优化

--- Sora/Module/UnitFrame/RaidAuras/Class.lua
+++ Sora/Module/UnitFrame/RaidAuras/Class.lua
@@ -17,30 +17,38 @@ local function OnPlayerLogin(self, event, ...)
         -- do nothing
     end
 
-    -- 各职业减伤/保命技能
+    -- 各职业减伤、保命技能
     do
-        GridStatusRaidDebuff:DebuffId(zoneid, 642, 1) -- 圣盾术
-        GridStatusRaidDebuff:DebuffId(zoneid, 871, 1) -- 盾墙
-        GridStatusRaidDebuff:DebuffId(zoneid, 1022, 1) -- 保护祝福
-        GridStatusRaidDebuff:DebuffId(zoneid, 27827, 1) -- 救赎之魂
-        GridStatusRaidDebuff:DebuffId(zoneid, 31224, 1) -- 暗影斗篷
-        GridStatusRaidDebuff:DebuffId(zoneid, 33206, 1) -- 痛苦压制
-        GridStatusRaidDebuff:DebuffId(zoneid, 45438, 1) -- 冰箱
-        GridStatusRaidDebuff:DebuffId(zoneid, 47585, 1) -- 消散
-        GridStatusRaidDebuff:DebuffId(zoneid, 47788, 1) -- 守护之魂
-        GridStatusRaidDebuff:DebuffId(zoneid, 48792, 1) -- 冰封之韧
-        GridStatusRaidDebuff:DebuffId(zoneid, 86659, 1) -- 远古列王守卫
-        GridStatusRaidDebuff:DebuffId(zoneid, 102342, 1) -- 铁木树皮
-        GridStatusRaidDebuff:DebuffId(zoneid, 104773, 1) -- 不灭决心
-        GridStatusRaidDebuff:DebuffId(zoneid, 108271, 1) -- 星界转移
+        -- MONK
         GridStatusRaidDebuff:DebuffId(zoneid, 115203, 1) -- 壮胆酒
-        GridStatusRaidDebuff:DebuffId(zoneid, 116849, 1) -- 作茧缚命
-        GridStatusRaidDebuff:DebuffId(zoneid, 118038, 1) -- 剑在人在
-        GridStatusRaidDebuff:DebuffId(zoneid, 160029, 1) -- 正在复活
-        GridStatusRaidDebuff:DebuffId(zoneid, 186265, 1) -- 灵龟守护
-        GridStatusRaidDebuff:DebuffId(zoneid, 196555, 1) -- 虚空行走
-        GridStatusRaidDebuff:DebuffId(zoneid, 204018, 1) -- 破咒祝福
-        GridStatusRaidDebuff:DebuffId(zoneid, 204150, 1) -- 圣光护盾
+        GridStatusRaidDebuff:DebuffId(zoneid, 116849, 2) -- 作茧缚命
+
+        -- DRUID
+        GridStatusRaidDebuff:DebuffId(zoneid, 61336, 1) -- 生存本能
+        GridStatusRaidDebuff:DebuffId(zoneid, 22812, 2) -- 树皮术
+        GridStatusRaidDebuff:DebuffId(zoneid, 22842, 3) -- 狂暴回复
+        GridStatusRaidDebuff:DebuffId(zoneid, 102342, 4) -- 铁木树皮
+
+        -- PRIEST
+        GridStatusRaidDebuff:DebuffId(zoneid, 47788, 1) -- 守护之魂
+        GridStatusRaidDebuff:DebuffId(zoneid, 33206, 2) -- 痛苦压制
+
+        -- PALADIN
+        GridStatusRaidDebuff:DebuffId(zoneid, 642, 1) -- 圣盾术
+        GridStatusRaidDebuff:DebuffId(zoneid, 1022, 2) -- 保护祝福
+        GridStatusRaidDebuff:DebuffId(zoneid, 204018, 3) -- 破咒祝福
+        GridStatusRaidDebuff:DebuffId(zoneid, 86659, 4) -- 远古列王守卫
+        GridStatusRaidDebuff:DebuffId(zoneid, 204150, 5) -- 圣光护盾
+
+        -- WARRIOR
+        GridStatusRaidDebuff:DebuffId(zoneid, 871, 1) -- 盾墙
+        GridStatusRaidDebuff:DebuffId(zoneid, 12975, 2) -- 破釜沉舟
+
+        -- DEMONHUNTER
+        GridStatusRaidDebuff:DebuffId(zoneid, 187827, 4) -- 恶魔变形
+
+        -- DEATHKNIGHT
+        GridStatusRaidDebuff:DebuffId(zoneid, 48792, 1) -- 冰封之韧
     end
 end
 
