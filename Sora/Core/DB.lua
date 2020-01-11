934eb2a2c458a67591b40719e2d2310979c419f3
neavo <neavo@neavo.me>
2020-01-11 22:22:01 +0800

    Plugins : 集成镜头景深效果

--- Sora/Core/DB.lua
+++ Sora/Core/DB.lua
@@ -2,9 +2,10 @@
 local S, C, L, DB = unpack(select(2, ...))
 
 -- Begin
-DB.Border = "InterFace\\AddOns\\Sora\\Media\\Border"
-DB.GlowTex = "InterFace\\AddOns\\Sora\\Media\\GlowTex"
+DB.Border = "Interface\\AddOns\\Sora\\Media\\Border"
+DB.GlowTex = "Interface\\AddOns\\Sora\\Media\\GlowTex"
 DB.Backdrop = "Interface\\ChatFrame\\ChatFrameBackground"
-DB.Statusbar = "InterFace\\AddOns\\Sora\\Media\\Statusbar"
+DB.Statusbar = "Interface\\AddOns\\Sora\\Media\\Statusbar"
+DB.BorderShadow = "Interface\\AddOns\\Sora\\Media\\BorderShadow"
 
 DB.AuraFont = "Interface\\Addons\\Sora\\Media\\ROADWAY.TTF"
---
+++ Sora/Module/Plugins/BorderShadow.lua
@@ -0,0 +1,20 @@
+﻿-- Engine
+local S, C, L, DB = unpack(select(2, ...))
+
+-- Event
+local function OnPlayerLogin(self, event, ...)
+    local frame = CreateFrame("Frame", nil, UIParent)
+    frame:SetAlpha(0.50)
+    frame:SetAllPoints()
+    frame:SetFrameLevel(0)
+    frame:SetFrameStrata("BACKGROUND")
+
+    frame.bg = frame:CreateTexture(nil, "BORDER")
+    frame.bg:SetAllPoints()
+    frame.bg:SetTexture(DB.BorderShadow)
+end
+
+-- Handler
+local EventHandler = S.CreateEventHandler()
+EventHandler.Event.PLAYER_LOGIN = OnPlayerLogin
+EventHandler.Register()
