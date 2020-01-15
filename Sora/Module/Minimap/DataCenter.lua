99cee378d5c5719502ff9d779dbd55dc32f94c91
neavo <neavo@neavo.me>
2020-01-15 23:37:14 +0800

    Minimap : 修正数据中心显示逻辑的问题

--- Sora/Module/Minimap/DataCenter.lua
+++ Sora/Module/Minimap/DataCenter.lua
@@ -1,37 +1,21 @@
 -- Engines
 local S, C, L, DB = unpack(select(2, ...))
 
--- Begin
-local function UpdateAnchors(self, ...)
-    local lines = {}
+-- Common
+local function IsPlayerMaxLevel()
+    local maxLevel = GetRestrictedAccountData()
 
-    table.insert(lines, anchors.First)
-    table.insert(lines, anchors.Second)
-
-    if UnitLevel("player") ~= MAX_PLAYER_LEVEL then
-        table.insert(lines, anchors.Experience)
+    if (maxLevel == 0) then
+        maxLevel = MAX_PLAYER_LEVEL_TABLE[GetExpansionLevel()]
     end
 
-    if select(1, GetWatchedFactionInfo()) then
-        table.insert(lines, anchors.Reputation)
-    end
-
-    if HasArtifactEquipped() or (C_AzeriteItem and C_AzeriteItem.FindActiveAzeriteItem()) then
-        table.insert(lines, anchors.ArtifactPower)
-    end
-
-    for k, v in pairs(lines) do
-        if k == 1 then
-            v:SetPoint("TOP")
-        else
-            v:SetPoint("TOP", lines[k - 1], "BOTTOM", 0, 0)
-        end
-    end
+    return maxLevel == UnitLevel("player")
 end
 
 -- Event
 local function OnPlayerLogin(self, event, ...)
     local unit = nil
+    local units = {}
     local row, yOffset = 0, -18
 
     row = row + 1
@@ -50,29 +34,45 @@ local function OnPlayerLogin(self, event, ...)
     unit:SetSize((Minimap:GetWidth() - 8) / 2, 6)
     unit:SetPoint("TOPRIGHT", Minimap, "BOTTOMRIGHT", 0, yOffset * row + 4)
 
-    if UnitLevel("player") ~= MAX_PLAYER_LEVEL then
-        row = row + 1
+    unit = S.Minimap.CreateExperience(Minimap)
+    unit:SetSize(Minimap:GetWidth(), 6)
+    table.insert(units, unit)
 
-        unit = S.Minimap.CreateExperience(Minimap)
-        unit:SetSize(Minimap:GetWidth(), 6)
-        unit:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
+    unit = S.Minimap.CreateReputation(Minimap)
+    unit:SetSize(Minimap:GetWidth(), 6)
+    table.insert(units, unit)
+
+    unit = S.Minimap.CreateArtifactPower(Minimap)
+    unit:SetSize(Minimap:GetWidth(), 6)
+    table.insert(units, unit)
+
+    local function OnTicker()
+        if InCombatLockdown() then
+            return 0
+        end
+
+        for k, v in pairs(units) do
+            v:ClearAllPoints()
+        end
+
+        row = 2
+        if not IsPlayerMaxLevel() then
+            row = row + 1
+            units[1]:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
+        end
+
+        if select(1, GetWatchedFactionInfo()) then
+            row = row + 1
+            units[2]:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
+        end
+
+        if HasArtifactEquipped() or C_AzeriteItem.FindActiveAzeriteItem() then
+            row = row + 1
+            units[3]:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
+        end
     end
 
-    if select(1, GetWatchedFactionInfo()) then
-        row = row + 1
-
-        unit = S.Minimap.CreateReputation(Minimap)
-        unit:SetSize(Minimap:GetWidth(), 6)
-        unit:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
-    end
-
-    if HasArtifactEquipped() or C_AzeriteItem.FindActiveAzeriteItem() then
-        row = row + 1
-
-        unit = S.Minimap.CreateArtifactPower(Minimap)
-        unit:SetSize(Minimap:GetWidth(), 6)
-        unit:SetPoint("TOP", Minimap, "BOTTOM", 0, yOffset * row + 4)
-    end
+    C_Timer.NewTicker(1.00, OnTicker)
 end
 
 -- Handler
