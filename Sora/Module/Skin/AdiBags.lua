2a636f6013b9f634a27fdcf51bf7eb0f6a7ec125
neavo <neavo@neavo.me>
2020-01-17 00:42:13 +0800

    Skin : 基本完成AdiBags皮肤

--- Sora/Module/Skin/AdiBags.lua
+++ Sora/Module/Skin/AdiBags.lua
@@ -2,75 +2,71 @@
 local S, C, L, DB = unpack(select(2, ...))
 
 -- Common
-local function SkinBag()
-	if not AdiBagsContainer1 then
-		return 0
+local function Dummy()
+end
+
+local function DoSkin(i)
+	local prefix = ""
+
+	if i == 1 then
+		prefix = "AdiBagsItemButton"
+	elseif i == 2 then
+		prefix = "AdiBagsBankItemButton"
 	end
 
 	for i = 1, 999 do
-		local btn = _G["AdiBagsItemButton" .. i]
+		local btn = _G[prefix .. i]
 
 		if not btn then
 			break
 		end
 
+		local icon = btn.IconTexture
+
 		if not btn.__skined then
-			btn.shadow = S.MakeShadow(btn, 2)
-			btn.shadow:SetFrameLevel(btn:GetFrameLevel())
+			local count = btn.Count
+			local normalTex = btn.NormalTexture
+			local iconQuestTex = btn.IconQuestTexture
+
+			btn.__skined = true
+			btn.border = S.MakeBorder(btn, 1)
+			btn.border:SetFrameLevel(btn:GetFrameLevel() + 1)
 
 			local p = C.Core.Pixel or 1.00
-			local count = _G["AdiBagsItemButton" .. i .. "Count"]
 			count:SetPoint("BOTTOMRIGHT", 0, 0)
-			count:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+			count:SetFont(STANDARD_TEXT_FONT, 11, "OUTLINE")
 			count:SetShadowOffset(1.00 * p, -1.00 * p)
 			count:SetShadowColor(0.00, 0.00, 0.00, 0.50)
 
-			S.KillFrame(_G["AdiBagsItemButton" .. i .. "NormalTexture"])
-			S.KillFrame(_G["AdiBagsItemButton" .. i .. "IconQuestTexture"])
+			icon.__SetTexCoord = icon.SetTexCoord
+			icon.SetTexCoord = Dummy
+
+			S.KillFrame(normalTex)
+			S.KillFrame(iconQuestTex)
 		end
 
-		local itemid = btn.itemId
-		local itemName, itemLink, itemRarity = GetItemInfo(itemid or 6948)
-		local r, g, b = GetItemQualityColor(itemRarity)
+		local border = btn.border
+		local link = GetContainerItemLink(btn.bag or 999, btn.slot or 999)
 
-		btn.__skined = true
-		btn.shadow:SetBackdropBorderColor(r, g, b, 1.00)
+		if not link then
+			icon:__SetTexCoord(0.25, 0.75, 0.25, 0.75)
+			border:SetBackdropBorderColor(1.00, 1.00, 1.00, 1.00)
+		else
+			local _, _, color = string.find(link, "|?c?f?f?(%x*)|?H?")
+			local r, g, b = S.HexToRGB(color)
+
+			icon:__SetTexCoord(0.08, 0.92, 0.08, 0.92)
+			border:SetBackdropBorderColor(r, g, b, 1.00)
+		end
 	end
 end
 
+local function SkinBag()
+	DoSkin(1)
+end
+
 local function SkinBank()
-	if not AdiBagsContainer2 then
-		return 0
-	end
-
-	for i = 1, 999 do
-		local btn = _G["AdiBagsBankItemButton" .. i]
-
-		if not btn then
-			break
-		end
-
-		if not btn.__skined then
-			btn.shadow = S.MakeShadow(btn, 2)
-
-			local p = C.Core.Pixel or 1.00
-			local count = _G["AdiBagsBankItemButton" .. i .. "Count"]
-			count:SetPoint("BOTTOMRIGHT", 0, 0)
-			count:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-			count:SetShadowOffset(1.00 * p, -1.00 * p)
-			count:SetShadowColor(0.00, 0.00, 0.00, 0.50)
-
-			S.KillFrame(_G["AdiBagsBankItemButton" .. i .. "NormalTexture"])
-			S.KillFrame(btn.IconQuestTexture)
-		end
-
-		local itemid = btn.itemId
-		local itemName, itemLink, itemRarity = GetItemInfo(itemid or 6948)
-		local r, g, b = GetItemQualityColor(itemRarity)
-
-		btn.__skined = true
-		btn.shadow:SetBackdropBorderColor(r, g, b, 1.00)
-	end
+	DoSkin(2)
 end
 
 -- Event
@@ -88,9 +84,22 @@ local function OnPlayerLogin(self, event, ...)
 		return 0
 	end
 
-	for i = 1, 2 do
+	local datas = {
+		"AdiBagsContainer1",
+		"AdiBagsContainer2"
+	}
+
+	for k, v in pairs(datas) do
+		local function OnShowSkin()
+			DoSkin(k)
+		end
+
+		local function OnShow(self, ...)
+			C_Timer.NewTicker(0.25, OnShowSkin, 1)
+		end
+
 		local function OnTicker(self, ...)
-			local container = _G["AdiBagsContainer" .. i]
+			local container = _G[v]
 
 			if not container then
 				return 0
@@ -98,41 +107,19 @@ local function OnPlayerLogin(self, event, ...)
 
 			self:Cancel()
 
-			if 1 == 1 then
-				SkinBag()
-			elseif i == 2 then
-				SkinBank()
-			end
-
 			container.shadow = S.MakeShadow(container, 2)
 			container.shadow:SetFrameLevel(container:GetFrameLevel())
-			container.shadow:SetBackdropBorderColor(1.00, 1.00, 1.00, 1.00)
+
+			DoSkin(k)
+			container:HookScript("OnShow", OnShow)
 		end
 
 		C_Timer.NewTicker(1.00, OnTicker)
 	end
 end
 
-local function OnBankFrameOpened(self, event, ...)
-	if not IsAddOnLoaded("AdiBags") then
-		return 0
-	end
-
-	C_Timer.NewTicker(0.25, SkinBank, 1)
-end
-
-local function OnGuildBankFrameOpened(self, event, ...)
-	if not IsAddOnLoaded("AdiBags") then
-		return 0
-	end
-
-	C_Timer.NewTicker(0.25, SkinBank, 1)
-end
-
 -- Handler
 local EventHandler = S.CreateEventHandler()
 EventHandler.Event.BAG_UPDATE = OnBagUpdate
 EventHandler.Event.PLAYER_LOGIN = OnPlayerLogin
-EventHandler.Event.BANKFRAME_OPENED = OnBankFrameOpened
--- EventHandler.Event.GUILDBANKFRAME_OPENED = OnGuildBankFrameOpened
 EventHandler.Register()
