e06c91bcf6d1c2d3c05bd98f90ebfd3f1bd315e9
neavo <neavo@neavo.me>
2020-01-17 17:49:13 +0800

    Tutorial : 添加对Mapster的初始化设置

---
+++ Sora/Module/Tutorial/Plugin/Mapster.lua
@@ -0,0 +1,43 @@
+﻿-- Engine
+local S, C, L, DB = unpack(select(2, ...))
+
+-- Event
+local function OnPlayerLogin(self, event, ...)
+	if C.Tutorial.Initialized then
+		return 0
+	end
+
+	MapsterDB = {
+		["namespaces"] = {
+			["FogClear"] = {
+				["profiles"] = {
+					["Default"] = {
+						["colorB"] = 0,
+						["colorA"] = 0.50,
+						["colorG"] = 0,
+						["colorR"] = 0
+					}
+				}
+			},
+			["Coords"] = {
+				["profiles"] = {
+					["Default"] = {
+						["accuracy"] = 2
+					}
+				}
+			}
+		},
+		["profiles"] = {
+			["Default"] = {
+				["ejScale"] = 1.00,
+				["scale"] = 1.20,
+				["poiScale"] = 1.00
+			}
+		}
+	}
+end
+
+-- Handler
+local EventHandler = S.CreateEventHandler()
+EventHandler.Event.PLAYER_LOGIN = OnPlayerLogin
+EventHandler.Register()
