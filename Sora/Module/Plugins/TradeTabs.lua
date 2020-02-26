c2a4881f5977cee080f166770c143c5a29e06037
neavo <neavo@neavo.me>
2020-02-26 22:11:16 +0800

    Plugins : 添加了专业标签聚合的功能

---
+++ Sora/Module/Plugins/TradeTabs.lua
@@ -0,0 +1,47 @@
+-- Engine
+local S, C, L, DB = unpack(select(2, ...))
+
+-- Event
+local function OnTradeSkillShow(self, event, ...)
+	local datas = {}
+	local professions = {GetProfessions()}
+
+	for k, v in pairs(professions) do
+		local name, icon, _, _, _, spelloffset = GetProfessionInfo(v)
+
+		local data = {}
+		data.icon = icon
+		data.spelloffset = spelloffset
+
+		table.insert(datas, data)
+	end
+
+	for k, v in pairs(datas) do
+		local button = S.CreateButton(TradeSkillFrame, 20)
+		button:SetSize(36, 36)
+		button:SetFrameLevel(TradeSkillFrame:GetFrameLevel())
+
+		button.icon = button:CreateTexture(nil, "ARTWORK")
+		button.icon:SetSize(36, 36)
+		button.icon:SetPoint("CENTER", button, "CENTER", 0, 0)
+		button.icon:SetTexture(v.icon)
+		button.icon:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+
+		local function OnClick(self, btn, ...)
+			CastSpell(v.spelloffset + 1, BOOKTYPE_PROFESSION)
+		end
+
+		button:HookScript("OnClick", OnClick)
+
+		if k == 1 then
+			button:SetPoint("TOPLEFT", TradeSkillFrame, "TOPRIGHT", 8, 0)
+		else
+			button:SetPoint("TOPLEFT", TradeSkillFrame, "TOPRIGHT", 8, -(36 + 8) * (k - 1))
+		end
+	end
+end
+
+-- Handler
+local EventHandler = S.CreateEventHandler()
+EventHandler.Event.TRADE_SKILL_SHOW = OnTradeSkillShow
+EventHandler.Register()
