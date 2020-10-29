769a6360c09f1c85e752f1d06dbcf4aa51561dde
neavo <neavo@neavo.me>
2020-10-29 22:51:02 +0800

    动作条：调整样式

--- Sora/Module/ActionBar/Style.lua
+++ Sora/Module/ActionBar/Style.lua
@@ -16,17 +16,48 @@ local function OnPlayerLogin(self, event, ...)
             points = {{"TOPLEFT", -2 * p, 2 * p}, {"BOTTOMRIGHT", 2 * p, -2 * p}}
         },
         icon = {texCoord = {0.08, 0.92, 0.08, 0.92}},
-        flash = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        flyoutBorder = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        flyoutBorderShadow = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        border = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        normalTexture = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        pushedTexture = {file = "", texCoord = {0.08, 0.92, 0.08, 0.92}},
-        checkedTexture = {texCoord = {0.08, 0.92, 0.08, 0.92}},
-        highlightTexture = {texCoord = {0.08, 0.92, 0.08, 0.92}},
-        name = {font = {STANDARD_TEXT_FONT, 10, "OUTLINE"}, points = {{"BOTTOM", 0, 1}}},
-        count = {font = {STANDARD_TEXT_FONT, 12, "OUTLINE"}, points = {{"TOPLEFT", 0, 0}}},
-        hotkey = {font = {STANDARD_TEXT_FONT, 12, "OUTLINE"}, points = {{"TOPRIGHT", 0, -1}}}
+        flash = {
+            file = "",
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        flyoutBorder = {
+            file = "",
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        flyoutBorderShadow = {
+            file = "",
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        border = {
+            file = "",
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        normalTexture = {
+            file = "",
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        shine = {
+            points = {{"TOPLEFT", 0.00, 0.00}, {"BOTTOMRIGHT", 0.00, 0.00}}
+        },
+        autocastable = {
+            texCoord = {0.22, 0.78, 0.22, 0.78},
+            points = {{"TOPLEFT", -2 * p, 2 * p}, {"BOTTOMRIGHT", 2 * p, -2 * p}}
+        },
+        highlightTexture = {
+            texCoord = {0.08, 0.92, 0.08, 0.92}
+        },
+        name = {
+            font = {STANDARD_TEXT_FONT, 10, "OUTLINE"},
+            points = {{"BOTTOM", 0, 1}}
+        },
+        count = {
+            font = {STANDARD_TEXT_FONT, 12, "OUTLINE"},
+            points = {{"TOPLEFT", 0, 0}}
+        },
+        hotkey = {
+            font = {STANDARD_TEXT_FONT, 12, "OUTLINE"},
+            points = {{"TOPRIGHT", 0, -1}}
+        }
     }
 
     rButtonTemplate:StyleAllActionButtons(style)
