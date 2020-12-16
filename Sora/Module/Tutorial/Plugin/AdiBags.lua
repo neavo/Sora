51b2dfabb1e712006beacda5dc87ac8fa33c0301
neavo <neavo@neavo.me>
2020-12-16 12:06:50 +0800

    设置向导 : 更新第三方插件的默认设置

--- Sora/Module/Tutorial/Plugin/AdiBags.lua
+++ Sora/Module/Tutorial/Plugin/AdiBags.lua
@@ -9,14 +9,13 @@ local function OnPlayerLogin(self, event, ...)
 
 	AdiBagsDB = {
 		["namespaces"] = {
-			["ItemLevel"] = {
-			},
+			["ItemLevel"] = {},
 			["FilterOverride"] = {
 				["profiles"] = {
 					["Default"] = {
-						["version"] = 3,
-					},
-				},
+						["version"] = 3
+					}
+				}
 			},
 			["ItemCategory"] = {
 				["profiles"] = {
@@ -26,24 +25,24 @@ local function OnPlayerLogin(self, event, ...)
 							["消耗品"] = true,
 							["杂项"] = true,
 							["商业技能"] = true,
-							["宝石"] = true,
 							["配方"] = true,
-						},
-					},
-				},
+							["宝石"] = true
+						}
+					}
+				}
 			},
 			["NewItem"] = {
 				["profiles"] = {
 					["Default"] = {
 						["highlight"] = "none",
-						["glowScale"] = 1.00,
-					},
-				},
+						["glowScale"] = 1
+					}
+				}
 			},
-			["AdiBags_TooltipInfo"] = {
-			},
-			["Equipment"] = {
-			},
+			["AdiBags_TooltipInfo"] = {},
+			["MoneyFrame"] = {},
+			["DataSource"] = {},
+			["ItemSets"] = {},
 			["CurrencyFrame"] = {
 				["profiles"] = {
 					["Default"] = {
@@ -54,98 +53,88 @@ local function OnPlayerLogin(self, event, ...)
 							["腐化的纪念品"] = false,
 							["海员达布隆币"] = false,
 							["虚空碎片"] = false,
-							["凝结幻象"] = false,
+							["要塞物资"] = false,
 							["古怪硬币"] = false,
 							["黯淡的阿古尼特水晶"] = false,
-							["次级好运护符"] = false,
-							["上古神器"] = false,
-							["泰坦残血精华"] = false,
-							["黑暗之尘"] = false,
-							["觉醒精华"] = false,
 							["战争物资"] = false,
-							["破碎命运印记"] = false,
-							["尼奥罗萨的回响"] = false,
-							["棱彩法力珍珠"] = false,
+							["上古神器"] = false,
+							["神器碎片"] = false,
+							["黑暗之尘"] = false,
+							["死亡之翼的堕落精华"] = false,
+							["时空扭曲徽章"] = false,
+							["幽魂碎片"] = false,
+							["埃匹希斯水晶"] = false,
+							["凝结幻象"] = false,
 							["原油"] = false,
 							["职业大厅资源"] = false,
 							["远古魔力"] = false,
 							["徘徊之魂碎片"] = false,
-							["埃匹希斯水晶"] = false,
-							["幽魂碎片"] = false,
-							["时空扭曲徽章"] = false,
-							["死亡之翼的堕落精华"] = false,
-							["神器碎片"] = false,
+							["棱彩法力珍珠"] = false,
+							["尼奥罗萨的回响"] = false,
+							["破碎命运印记"] = false,
+							["觉醒精华"] = false,
+							["泰坦残血精华"] = false,
 							["第七军团服役勋章"] = false,
-							["要塞物资"] = false,
+							["次级好运护符"] = false
 						},
 						["text"] = {
-							["name"] = "默认",
-						},
-					},
-				},
+							["name"] = "默认"
+						}
+					}
+				}
 			},
-			["DataSource"] = {
-			},
-			["ItemSets"] = {
-			},
-			["Junk"] = {
-			},
-			["MoneyFrame"] = {
-			},
+			["Junk"] = {},
+			["Equipment"] = {}
 		},
-		["profileKeys"] = {
-		},
+		["profileKeys"] = {},
 		["profiles"] = {
 			["Default"] = {
 				["virtualStacks"] = {
 					["freeSpace"] = false,
+					["others"] = true,
+					["stackable"] = true,
+					["incomplete"] = true
 				},
-				["columnWidth"] = {
-					["Backpack"] = 16,
-					["Bank"] = 16,
-				},
+				["scale"] = 1,
 				["skin"] = {
 					["ReagentBankColor"] = {
-						0.00, -- [1]
-						0.00, -- [2]
-						0.00, -- [3]
-						0.50 -- [4]
+						nil, -- [1]
+						0, -- [2]
+						nil, -- [3]
+						0.5 -- [4]
 					},
 					["BackpackColor"] = {
-						0.00, -- [1]
-						0.00, -- [2]
-						0.00, -- [3]
-						0.50 -- [4]
+						nil, -- [1]
+						nil, -- [2]
+						nil, -- [3]
+						0.5 -- [4]
 					},
 					["BankColor"] = {
-						0.00, -- [1]
-						0.00, -- [2]
-						0.00, -- [3]
-						0.50 -- [4]
+						nil, -- [1]
+						nil, -- [2]
+						0, -- [3]
+						0.5 -- [4]
 					},
-					["borderWidth"] = 1,
-					["background"] = "Solid",
 					["border"] = "None",
-					["insets"] = 0,
+					["background"] = "Solid",
+					["borderWidth"] = 1,
+					["insets"] = 0
 				},
 				["bagFont"] = {
-					["size"] = 10,
-				},
-				["positions"] = {
-					["Backpack"] = {
-						["xOffset"] = -119.5,
-						["yOffset"] = 225.5000305175781,
-					},
+					["size"] = 10
 				},
 				["compactLayout"] = true,
 				["hideAnchor"] = true,
 				["sectionFont"] = {
-					["size"] = 13,
+					["size"] = 13
 				},
 				["positionMode"] = "manual",
-				["scale"] = 1,
-			},
-		},
+				["columnWidth"] = {
+					["Bank"] = 16,
+					["Backpack"] = 16
+				}
+			}
+		}
 	}
 end
 
