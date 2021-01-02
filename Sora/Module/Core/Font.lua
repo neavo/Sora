856cc4591c276628612bab3f82b69c68669f696e
neavo <neavo@neavo.me>
2021-01-02 12:24:06 +0800

    update style

--- Sora/Module/Core/Font.lua
+++ Sora/Module/Core/Font.lua
@@ -6,7 +6,7 @@ local function IsTextRegion(k, v)
 	return type(v) == "table" and v.GetObjectType and v:GetObjectType() == "Font" and v.GetFont
 end
 
-local function ProcessTextRegion(fontString, scale)
+local function ProcessTextRegion(fontString, scale, color)
 	if fontString.__Processed then
 		return 0
 	end
@@ -24,6 +24,10 @@ local function ProcessTextRegion(fontString, scale)
 	fontString:SetShadowColor(0.00, 0.00, 0.00, s and 1.00 or 0.00)
 	fontString:SetFont(STANDARD_TEXT_FONT, size * scale, o and "OUTLINE" or nil)
 
+	if color ~= nil then
+		fontString:SetTextColor(unpack(color))
+	end
+
 	fontString.__Processed = true
 end
 
@@ -37,68 +41,34 @@ do
 	hooksecurefunc("PaperDollFrame_SetLabelAndText", HookPaperDollFrameSetLabelAndText)
 end
 
--- Special for Blizzard_WarboardUI、Blizzard_QuestChoice
-local function SetTextColor(r, g, b, a)
-	-- do nothing
-end
-local function HookQuestChoiceFrameUpdate()
-	local regions = S.GetRegions(QuestChoiceFrame, "FontString")
-
-	for k, v in pairs(regions) do
-		ProcessTextRegion(v, 1.00)
-
-		v:SetText((v:GetText() or ""):gsub("\r+", ""):gsub("\n+", ""))
-		v:SetSpacing(2)
-		v:SetTextColor(0.90, 0.90, 0.90, 1.00)
-		v.SetTextColor = SetTextColor
-	end
-end
-local function HookWarboardQuestChoiceFrameUpdate()
-	local regions = S.GetRegions(WarboardQuestChoiceFrame, "FontString")
-
-	for k, v in pairs(regions) do
-		ProcessTextRegion(v, 1.00)
-
-		v:SetText((v:GetText() or ""):gsub("\r+", ""):gsub("\n+", ""))
-		v:SetSpacing(2)
-		v:SetTextColor(0.90, 0.90, 0.90, 1.00)
-		v.SetTextColor = SetTextColor
-	end
-end
-local function OnBlizzardWarboardUILoad(self, event, name, ...)
-	hooksecurefunc(WarboardQuestChoiceFrame, "Update", HookWarboardQuestChoiceFrameUpdate)
-end
-local function OnBlizzardQuestChoiceLoad(self, event, name, ...)
-	hooksecurefunc(QuestChoiceFrame, "Update", HookQuestChoiceFrameUpdate)
-end
-
 -- Event
-local function OnAddonLoaded(self, event, name, ...)
-	if name == "Blizzard_WarboardUI" then
-		OnBlizzardWarboardUILoad(self, event, name, ...)
-	elseif name == "Blizzard_QuestChoice" then
-		OnBlizzardQuestChoiceLoad(self, event, name, ...)
-	end
-end
-
 local function OnPlayerLogin(self, event, ...)
 	local extras = {
-		"CombatTextTemplate"
+		["CombatTextTemplate"] = true
 	}
 
-	local specials = {
+	local specialScale = {
 		["GameFontNormalSmall"] = 0.80
+	}
+
+	local specialColor = {
+		["GameFontBlack"] = {0.90, 0.90, 0.90, 1.00},
+		["GameFontBlackMedium"] = {0.90, 0.90, 0.90, 1.00},
+		["GameFontBlackSmall"] = {0.90, 0.90, 0.90, 1.00},
+		["GameFontBlackSmall2"] = {0.90, 0.90, 0.90, 1.00},
+		["GameFontBlackTiny"] = {0.90, 0.90, 0.90, 1.00},
+		["GameFontBlackTiny2"] = {0.90, 0.90, 0.90, 1.00}
 	}
 
 	for k, v in pairs(_G) do
 		if string.match(k, "Font") and IsTextRegion(k, v) then
-			ProcessTextRegion(v, specials[k] or 0.95)
+			ProcessTextRegion(v, specialScale[k] or 0.95, specialColor[k] or nil)
 		end
 	end
 
 	for k, v in pairs(extras) do
 		if IsTextRegion(k, v) then
-			ProcessTextRegion(_G[v], specials[v] or 0.95)
+			ProcessTextRegion(_G[k], specialScale[k] or 0.95, specialColor[k] or nil)
 		end
 	end
 end
